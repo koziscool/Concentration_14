5784ec2c8d13e25a9f16a74e0ad951c3a1c83ba4
John Kosmicke <kozjob@yahoo.com>
2018-03-11 19:09:03 -0700

    update

--- view.js
+++ view.js
@@ -2,8 +2,32 @@
 var matcherView = {
 	model: matcherModel,
 
-	init: function() {},
-	
+	init: function() {
+		this.$grid = $("grid-matcher");
+		this.addCardsToGrid();
+
+		width = 100.0 / this.model.size - 2;
+		$(".card").css({
+			width: width + "%"
+		});
+
+		$(".card").click( function(){
+			matcherController.selectCard($(this).data('card-id'));
+		});
+
+	},
+
+	addCardsToGrid: function(){
+		for(var i = 0; i < this.model.cards.length; i++){
+			var card = this.model.cards[i];
+			$cardDiv= $('<div><div class="name">' + card.value + '</div></div>');
+			$cardDiv.addClass('card')
+			$cardDiv.attr('id', card.id);
+			$cardDiv.data('card-id', card.id);
+			this.$grid.appendChild( $cardDiv );
+		}
+	}
+
 	updateGameView: function() {},
 };
 
